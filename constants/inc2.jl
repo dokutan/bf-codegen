29d6f468f303fae8a62fa2613c074fd5d7120b79
dokutan <54861821+dokutan@users.noreply.github.com>
2023-09-02 20:52:08 +0200

    Improve inc2.jl performance

--- constants/inc2.jl
+++ constants/inc2.jl
@@ -3,73 +3,89 @@
 # searches for the optimal parameters to increment a cell with one loop:
 # i1 (?) > il (L) [ < a1 > al ]
 
-function bf_plus(a, b)
+#using StatProfilerHTML
+using StaticArrays
+
+function bf_plus(a::Int, b::Int)::Int
     r = a + b
     if r < 0
-        r  = 256 + r
+        r = 256 + r
     end
 
     return r % 256
 end
 
-function simulate(i1, L, il, a1, al)
+function simulate(i1::Int, L::Int, il::Int, a1::Int, al::Int)::Tuple{Bool,Int}
     r = i1
     l = bf_plus(L, il)
 
-    l_history = [l]
+    l_history = BitSet(l)
     while l != 0
         r = bf_plus(r, a1)
-        l  = bf_plus(l,  al)
+        l = bf_plus(l, al)
 
         if l in l_history
-            return Nothing
+            return false, 0
         end
 
         push!(l_history, l)
     end
 
-    return r
+    return true, r
 end
 
 function fitness(parms)
     return sum(map(abs, parms))
 end
 
+function inc2()
+    total_iterations = 21 * 256 * 21 * 21 * 20
+    println("total:   ", total_iterations)
 
-total_iterations = 21 * 256 * 21 * 21 * 20
-println("total:   ", total_iterations)
+    i::Int = 0
+    results = Dict()
+    for i1::Int in [-10:10;]
+        for L::Int in [0:255;]
+            for il::Int in [-10:10;]
+                for a1::Int in [-10:10;]
+                    for al::Int in [-10:-1; 1:10]
+                        has_result::Bool, r::Int = simulate(i1, L, il, a1, al)
+                        i += 1
 
-i = 0
-results = Dict()
-for i1 in [-10:10;]
-    for L in [0:255;]
-        for il in [-10:10;]
-            for a1 in [-10:10;]
-                for al in [-10:-1;1:10;]
-                    r = simulate(i1, L, il, a1, al)
-                    global i += 1
-
-                    if r != Nothing && r >= 0
-                        if haskey(results, [r, L])
-                            if fitness([i1, il, a1, al]) < fitness(results[[r, L]])
-                                results[[r, L]] = [i1, il, a1, al]
+                        if has_result && r >= 0
+                            if haskey(results, SA[r, L])
+                                if fitness(SA[i1, il, a1, al]) < fitness(results[SA[r, L]])
+                                    results[SA[r, L]] = SA[i1, il, a1, al]
+                                end
+                            else
+                                results[SA[r, L]] = SA[i1, il, a1, al]
                             end
-                        else
-                            results[[r, L]] = [i1, il, a1, al]
+                        end
+
+                        if i % 1000 == 0
+                            print("\rcurrent: ", i)
                         end
                     end
-
-                    print("\rcurrent: ", i)
                 end
             end
         end
     end
+    println()
+
+    result_keys = collect(keys(results))
+    sort!(result_keys)
+    f = open("inc2.csv", "w")
+    for r_key in result_keys
+        write(
+            f,
+            join(map(string, r_key), ',') *
+            "," *
+            join(map(string, results[r_key]), ',') *
+            "\n",
+        )
+    end
+    close(f)
 end
 
-result_keys = collect(keys(results))
-sort!(result_keys)
-f = open("inc2.csv", "w")
-for r_key in result_keys
-	write(f, join(map(string, r_key), ',') * "," * join(map(string, results[r_key]), ',') * "\n")
-end
-close(f)
+#@profilehtml inc2()
+inc2()
