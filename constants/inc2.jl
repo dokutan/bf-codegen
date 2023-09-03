ddba88b3ad108b8bc5db65418d946f6c8e161f07
dokutan <54861821+dokutan@users.noreply.github.com>
2023-09-03 10:15:48 +0200

    Improve inc2.jl performance even further

--- constants/inc2.jl
+++ constants/inc2.jl
@@ -15,23 +15,55 @@ function bf_plus(a::Int, b::Int)::Int
     return r % 256
 end
 
-function simulate(i1::Int, L::Int, il::Int, a1::Int, al::Int)::Tuple{Bool,Int}
-    r = i1
-    l = bf_plus(L, il)
+function generate_simulation_cache()
+    simulation_cache = Dict()
 
-    l_history = BitSet(l)
-    while l != 0
-        r = bf_plus(r, a1)
-        l = bf_plus(l, al)
+    for L::Int in [0:255;]
+        for al::Int in [-10:10;]
+            l = L
+            l_history = BitSet(l)
+            iterations = 0
 
-        if l in l_history
-            return false, 0
+            while l != 0
+                l = bf_plus(l, al)
+                iterations += 1
+
+                if l in l_history
+                    iterations = -1
+                    break
+                end
+
+                push!(l_history, l)
+            end
+
+            global simulation_cache[SA[L, al]] = iterations
         end
-
-        push!(l_history, l)
     end
 
-    return true, r
+    return simulation_cache
+end
+
+function simulate(
+    simulation_cache::Dict,
+    i1::Int,
+    L::Int,
+    il::Int,
+    a1::Int,
+    al::Int,
+)::Tuple{Bool,Int}
+
+    l = bf_plus(L, il)
+    iterations = simulation_cache[SA[l, al]]
+
+    if iterations < 0
+        return false, 0
+    else
+        r = (i1 + iterations * a1) % 256
+        if r < 0
+            r += 256
+        end
+        return true, r
+    end
 end
 
 function fitness(parms)
@@ -39,6 +71,9 @@ function fitness(parms)
 end
 
 function inc2()
+    println("generating simulation cache ...")
+    simulation_cache = generate_simulation_cache()
+
     total_iterations = 21 * 256 * 21 * 21 * 20
     println("total:   ", total_iterations)
 
@@ -50,7 +85,8 @@ function inc2()
             for il::Int in [-10:10;]
                 for a1::Int in [-10:10;]
                     for al::Int in [-10:-1; 1:10]
-                        has_result::Bool, r::Int = simulate(i1, L, il, a1, al)
+                        has_result::Bool, r::Int =
+                            simulate(simulation_cache, i1, L, il, a1, al)
 
                         i += 1
                         if i % 1000 == 0
@@ -79,6 +115,7 @@ function inc2()
     end
     println()
 
+    println("writing results to inc2.csv ...")
     result_keys = collect(keys(results))
     sort!(result_keys)
     f = open("inc2.csv", "w")
